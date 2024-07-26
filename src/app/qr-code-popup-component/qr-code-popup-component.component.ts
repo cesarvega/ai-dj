1f3d2eb0a262bddc94985f4e1b20f4307e844ef2
Cesar Vega <cesarvega.col@gmail.com>
2024-07-26 07:11:04 -0400

    fix qr code fromat

--- src/app/qr-code-popup-component/qr-code-popup-component.component.ts
+++ src/app/qr-code-popup-component/qr-code-popup-component.component.ts
@@ -10,30 +10,30 @@ import * as QRCode from 'qrcode';
 })
 export class QrCodePopupComponentComponent {
   @Input() url: string = '';
-    qrCodeImage: string = '';
-    @Output() closePopup = new EventEmitter<void>();
-    constructor() { }
+  qrCodeImage: string = '';
+  @Output() closePopup = new EventEmitter<void>();
+  constructor() { }
 
-    ngOnInit(): void {
-        QRCode.toDataURL(this.url)
-            .then((url: string) => {
-                this.qrCodeImage = url;
-            })
-            .catch((err: any) => {
-                console.error(err);
-            });
-    }
-    close() {
-      this.closePopup.emit();
-    }
+  ngOnInit(): void {
+    QRCode.toDataURL(this.url)
+      .then((url: string) => {
+        this.qrCodeImage = url;
+      })
+      .catch((err: any) => {
+        console.error(err);
+      });
+  }
+  close() {
+    this.closePopup.emit();
+  }
 
-    onBackdropClick(event: MouseEvent) {
-        this.close();
-    }
+  onBackdropClick(event: MouseEvent) {
+    this.close();
+  }
 
-    onPopupClick(event: MouseEvent) {
-        // Prevents the click on the popup from closing it
-        // event.stopPropagation();
-        this.closePopup.emit();
-    }
+  onPopupClick(event: MouseEvent) {
+    // Prevents the click on the popup from closing it
+    // event.stopPropagation();
+    this.closePopup.emit();
+  }
 }
