fc01c9e095917e3471e45778ff0b1d1a7f503ecc
morales.william <morales.william@correounivalle.edu.co>
2024-08-23 17:35:26 +0000

    fix audios sample from card audio books

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,14 +7,14 @@ module.exports = {
         DEFAULT: "#00FFFC",
         300: "#D1D5DB", // Agrega esta línea para incluir `gray-300`
         // Puedes incluir más valores de la escala gris aquí si es necesario
-        500: "#6B7280",
+        
       },
       
       
       black: "#222222",
       white: "#FFFFFF",
       gray: "#F4F4F4",
-      yellow: "#F1FF72",
+      yellow: "#00FFFC",
       bgColor: "#0D0D0D",
       gray: {
         DEFAULT: "#252525",
