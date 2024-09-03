d4c0b17df7c6783b3a544470fc936ffba79b3fe9
Cesar Vega <cesarvega.col@gmail.com>
2024-09-03 12:09:35 -0400

    save changes

--- src/app/book-store/landing-sample-universal/landing-sample-universal.component.ts
+++ src/app/book-store/landing-sample-universal/landing-sample-universal.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
-
+import { Component, } from '@angular/core';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-landing-sample-universal',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './landing-sample-universal.component.html',
   styleUrl: './landing-sample-universal.component.scss'
 })
 export class LandingSampleUniversalComponent {
 
+  hours: number = 1; // Establece las horas iniciales aquí
+  minutes: number = 0;
+  seconds: number = 0;
+
+  
 }
